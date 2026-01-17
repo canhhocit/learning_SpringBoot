0a32dc59c67d04a42ae38a13afbadded8e67bfe3
canhhocit <canhhuu26@gmail.com>
2026-01-17 21:58:13 +0700

    add learning

---
+++ learn00/learn00/src/main/java/com/canhhocit/learn00/Learn00Application.java
@@ -0,0 +1,13 @@
+package com.canhhocit.learn00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learn00Application.class, args);
+	}
+
+}
---
+++ learn00/learn00/src/test/java/com/canhhocit/learn00/Learn00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canhhocit.learn00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
